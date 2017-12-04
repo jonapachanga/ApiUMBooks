f05974104f6f8fc5fd781b04ee7df8e03bf0f550
jonapachanga <jonatan.eseiza@gmail.com>
2017-12-04 20:01:23 -0300

    Se subieron cambios

--- src/main/java/ar/edu/um/umbooks/gson2/App.java
+++ src/main/java/ar/edu/um/umbooks/gson2/App.java
@@ -1,35 +1,44 @@
 package ar.edu.um.umbooks.gson2;
 
+import java.io.ObjectOutput;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import ar.edu.um.umbooks.persistencia.jpa.JPAHelper;//
+import ar.edu.um.umbooks.persistencia.jpa.impl.DAOImpl;
+import ar.edu.um.umbooks.persistencia.jpa.impl.VolumenInfoDAOImpl;
 import ar.edu.um.umbooks.clasesapi.Items;
 import ar.edu.um.umbooks.clasesapi.JSON;
+import ar.edu.um.umbooks.clasesapi.VolumeInfo;
 import ar.edu.um.umbooks.servicios.SearchServices;
 import ar.edu.um.umbooks.singleton.ApiProperty;
 
 public class App 
 {
-    public static void main( String[] args ) throws Exception  {
+	
+    @SuppressWarnings("null")
+	public static void main( String[] args ) throws Exception  {
     	try {
     		
+    		VolumenInfoDAOImpl objeto = null;
+    		
     		SearchServices<JSON> volumen = new SearchServices<JSON>() {};
-    		SearchServices<JSON> filtro = new SearchServices<JSON>() {};
+    		//SearchServices<JSON> filtro = new SearchServices<JSON>() {};
     		SearchServices<Items> id = new SearchServices<Items>() {};
     		
     		String key = ApiProperty.getInstance().getPropiedades("key");
     		
     		
     		String idBook = volumen.getVolumenSearch("harry potter").getItems().get(0).getId();
-    		String resultFilter = filtro.getFilterSearch("paid-ebooks", "harry potter")
-    				.getItems()
-    				.get(0)
-    				.getVolumeInfo()
-    				.getAuthors().get(0);
-    		String resultId = id.getIDSearch(idBook)
-    				.getVolumeInfo()
-    				.getTitle();
     		
-    		System.out.println(idBook);
-    		System.out.println(resultFilter);
-    		System.out.println(resultId);
+    		
+    		VolumeInfo volumeInfo = id.getIDSearch(idBook)
+    				.getVolumeInfo();
+    		
+    		objeto.create(volumeInfo);
+    		
+    		
     		System.out.println(key);
     		 
 		} catch (Exception e) {
